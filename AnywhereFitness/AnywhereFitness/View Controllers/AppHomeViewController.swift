5e082e677866e94de8062aa60a307cee0d07c381
dillonp23 <dillonp.lambda@gmail.com>
2019-10-21 14:36:34 -0700

    App homepage wired up with collection view (delegate, data source and custom cell). Ready for implementing back end data to collection view cell to display full list of available classes

---
+++ AnywhereFitness/AnywhereFitness/View Controllers/AppHomeViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AppHomeViewController.swift
+//  AnywhereFitness
+//
+//  Created by Dillon P on 10/21/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class AppHomeViewController: UIViewController {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension AppHomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "fitnessClassCell", for: indexPath) as? FitnessClassCollectionViewCell else { return UICollectionViewCell() }
+        
+        return UICollectionViewCell()
+    }
+    
+    
+}
