42215454fcdc7c1abca094d58d790011a757f1cc
dillonp23 <dillonp.lambda@gmail.com>
2019-10-24 18:49:38 -0700

    Add SearchCollectionViewCell Class

---
+++ AnywhereFitness/AnywhereFitness/Custom Views/SearchCollectionViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  SearchCollectionViewCell.swift
+//  AnywhereFitness
+//
+//  Created by Dillon P on 10/24/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class SearchCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var classTitleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeAndDurationLabel: UILabel!
+    
+    @IBOutlet weak var fitnessCategoryImageView: UIImageView!
+    
+    
+    
+    
+    
+    
+    @IBAction func signUpButtonTapped(_ sender: Any) {
+    }
+    
+}
