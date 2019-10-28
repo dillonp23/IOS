be9e10af84670bd124232861589f6daac7b7bd51
Joel Groomer <joelgroomer@gmail.com>
2019-10-28 15:15:22 -0700

    Added IBAction for save button and initial guard statement.

--- AnywhereFitness/AnywhereFitness/View Controllers/AddClassViewController.swift
+++ AnywhereFitness/AnywhereFitness/View Controllers/AddClassViewController.swift
@@ -23,6 +23,7 @@ class AddClassViewController: UIViewController {
     @IBOutlet weak var txtCapacity: UITextField!
     
     var fitClassController: FitClassController?
+    var startTime = Date()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +31,18 @@ class AddClassViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func saveTapped(_ sender: Any) {
+        guard let title = txtClassTitle.text, !title.isEmpty,
+            let duration = Int(txtDuration.text ?? "-1"), duration > 0,
+            let addr1 = txtAddr1.text, !addr1.isEmpty,
+            let city = txtCity.text, !city.isEmpty,
+            let state = txtState.text, !state.isEmpty,
+            let zip = txtZip.text, !zip.isEmpty,
+            let price = Double(txtPrice.text ?? "-1"), price > 0,
+            let capacity = Int(txtCapacity.text ?? "0"), capacity >= 0
+        else { return }
+        
+    }
     
 
 }
