f24e69b465763f5860948d7470a726ac90728532
Joel Groomer <joelgroomer@gmail.com>
2019-10-28 14:51:43 -0700

    Changed up the Add Class screen a bit, added a scroll view.

--- AnywhereFitness/AnywhereFitness/View Controllers/AddClassViewController.swift
+++ AnywhereFitness/AnywhereFitness/View Controllers/AddClassViewController.swift
@@ -10,21 +10,26 @@ import UIKit
 
 class AddClassViewController: UIViewController {
 
+    @IBOutlet weak var txtClassTitle: UITextField!
+    @IBOutlet weak var pkrStartTime: UIDatePicker!
+    @IBOutlet weak var txtDuration: UITextField!
+    @IBOutlet weak var pkrCategoryIntensity: UIPickerView!
+    @IBOutlet weak var txtAddr1: UITextField!
+    @IBOutlet weak var txtAddr2: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtPrice: UITextField!
+    @IBOutlet weak var txtCapacity: UITextField!
+    
+    var fitClassController: FitClassController?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    
 
 }
