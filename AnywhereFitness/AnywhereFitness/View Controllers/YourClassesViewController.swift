89edef12da9641c180a639e787148d3455ff9e2f
dillonp23 <dillonp.lambda@gmail.com>
2019-10-27 17:33:51 -0700

    Add YourClasses and AddClass view controller classes

---
+++ AnywhereFitness/AnywhereFitness/View Controllers/YourClassesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YourClassesViewController.swift
+//  AnywhereFitness
+//
+//  Created by Dillon P on 10/27/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class YourClassesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
