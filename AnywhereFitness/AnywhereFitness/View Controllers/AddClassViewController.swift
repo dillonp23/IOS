fd8fa0d4502bc7c9ecb55b196ad6fb7219731b23
Joel Groomer <joelgroomer@gmail.com>
2019-10-28 15:47:13 -0700

    Added method to create the class. Still need handlers for the pickers.

--- AnywhereFitness/AnywhereFitness/View Controllers/AddClassViewController.swift
+++ AnywhereFitness/AnywhereFitness/View Controllers/AddClassViewController.swift
@@ -23,7 +23,9 @@ class AddClassViewController: UIViewController {
     @IBOutlet weak var txtCapacity: UITextField!
     
     var fitClassController: FitClassController?
-    var startTime = Date()
+    var startTime: String?
+    var category = ""
+    var intensity = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,17 +34,37 @@ class AddClassViewController: UIViewController {
     }
     
     @IBAction func saveTapped(_ sender: Any) {
-        guard let title = txtClassTitle.text, !title.isEmpty,
+        guard let fitClassController = fitClassController,
+            let uid = UserController.shared.loggedInUser?.uid,
+            let metro = UserController.shared.loggedInUser?.metro,
+            let title = txtClassTitle.text, !title.isEmpty,
+            let startTime = startTime,
             let duration = Int(txtDuration.text ?? "-1"), duration > 0,
             let addr1 = txtAddr1.text, !addr1.isEmpty,
             let city = txtCity.text, !city.isEmpty,
             let state = txtState.text, !state.isEmpty,
             let zip = txtZip.text, !zip.isEmpty,
-            let price = Double(txtPrice.text ?? "-1"), price > 0,
-            let capacity = Int(txtCapacity.text ?? "0"), capacity >= 0
+            let price = Double(txtPrice.text ?? "-1"), price > 0
         else { return }
         
+        let addr2 = txtAddr2.text ?? ""
+        var capacity: Int?
+        if let capacityString = txtCapacity.text {
+            capacity = Int(capacityString)
+        }
+        
+        let fitClass = FitClass(classID: UUID().uuidString, instructor: uid, startTime: startTime, duration: duration, title: title, category: category, intensity: intensity, metro: metro, addr1: addr1, addr2: addr2, city: city, state: state, zip: zip, price: price, maxRegistrants: capacity)
+        
+        fitClassController.createOrUpdateFitClass(with: fitClass.representation) { created in
+            if created {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                let alert = UIAlertController(title: "Error", message: "We're sorry, an error occurred and we weren't able to save your class. Please try again.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            }
+        }
     }
+    
     
 
 }
