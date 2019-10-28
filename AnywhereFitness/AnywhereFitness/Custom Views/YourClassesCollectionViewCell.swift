e5bc837e219384cd7067a0de1a7ed8d06f44aa94
dillonp23 <dillonp.lambda@gmail.com>
2019-10-27 17:46:46 -0700

    Update new class UI to include start time and duration inputs

---
+++ AnywhereFitness/AnywhereFitness/Custom Views/YourClassesCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  YourClassesCollectionViewCell.swift
+//  AnywhereFitness
+//
+//  Created by Dillon P on 10/27/19.
+//  Copyright © 2019 Julltron. All rights reserved.
+//
+
+import UIKit
+
+class YourClassesCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var fitnessCategoryImage: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var categoryLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var timeAndDurationLabel: UILabel!
+    
+}
